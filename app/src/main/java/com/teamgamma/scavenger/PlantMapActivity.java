eff80f5934df0cd8f791d6ee11e1e5f10ff07ef2
jayakrishna duvvuri <jayakrishna duvvuri>
2017-02-28 02:12:51 -0500

    Added Sign Out Feature. Added Manage Account Page where you can change email/password and delete your account

--- app/src/main/java/com/teamgamma/scavenger/PlantMapActivity.java
+++ app/src/main/java/com/teamgamma/scavenger/PlantMapActivity.java
@@ -79,8 +79,7 @@ import android.view.MenuItem;
 public class PlantMapActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         OnConnectionFailedListener,
-        LocationListener,
-        View.OnClickListener
+        LocationListener
         {
     private Integer THRESHOLD = 2;
     private DelayAutoCompleteTextView geo_autocomplete;
@@ -90,7 +89,7 @@ public class PlantMapActivity extends AppCompatActivity implements OnMapReadyCal
     private Location mLastLocation;
     private LocationRequest mLocationRequest;
     private Marker mCurrLocationMarker;
-    private FirebaseAuth mAuth;
+
     private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "EmailPassword";
     private TextView mStatusTextView;
@@ -114,22 +113,7 @@ public class PlantMapActivity extends AppCompatActivity implements OnMapReadyCal
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-                // ...
-            }
-        };
         final Geocoder geocoder = new Geocoder(this);
         setContentView(R.layout.activity_plant_map);
 
@@ -296,207 +280,21 @@ public class PlantMapActivity extends AppCompatActivity implements OnMapReadyCal
                 }
             }
 
-            // [START on_start_add_listener]
-            @Override
-            public void onStart() {
-                super.onStart();
-                mAuth.addAuthStateListener(mAuthListener);
-            }
-            // [END on_start_add_listener]
-
-            // [START on_stop_remove_listener]
-            @Override
-            public void onStop() {
-                super.onStop();
-                if (mAuthListener != null) {
-                    mAuth.removeAuthStateListener(mAuthListener);
-                }
-            }
-            // [END on_stop_remove_listener]
-
-            private void createAccount(String email, String password) {
-                Log.d(TAG, "createAccount:" + email);
-                if (!validateForm()) {
-                    return;
-                }
-
-                showProgressDialog();
-
-                // [START create_user_with_email]
-                mAuth.createUserWithEmailAndPassword(email, password)
-
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
-                                if (!task.isSuccessful()) {
-                                    Toast.makeText(PlantMapActivity.this, R.string.auth_failed,
-                                            Toast.LENGTH_SHORT).show();
-                                }
-
-                                // [START_EXCLUDE]
-                                hideProgressDialog();
-                                // [END_EXCLUDE]
-                            }
-                        });
-                // [END create_user_with_email]
-            }
-
-            private void signIn(String email, String password) {
-                Log.d(TAG, "signIn:" + email);
-                if (!validateForm()) {
-                    return;
-                }
-
-                showProgressDialog();
-
-                // [START sign_in_with_email]
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
-
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
-                                if (!task.isSuccessful()) {
-                                    Log.w(TAG, "signInWithEmail:failed", task.getException());
-                                    Toast.makeText(PlantMapActivity.this, R.string.auth_failed,
-                                            Toast.LENGTH_SHORT).show();
-                                }
-
-                                // [START_EXCLUDE]
-                                if (!task.isSuccessful()) {
-                                    mStatusTextView.setText(R.string.auth_failed);
-                                }
-                                hideProgressDialog();
-                                // [END_EXCLUDE]
-                            }
-                        });
-                // [END sign_in_with_email]
-            }
-
             private void signOut() {
-                mAuth.signOut();
-                updateUI(null);
-            }
-
-            private void sendEmailVerification() {
-                // Disable button
-                findViewById(R.id.verify_email_button).setEnabled(false);
-
-                // Send verification email
-                // [START send_email_verification]
-                final FirebaseUser user = mAuth.getCurrentUser();
-                user.sendEmailVerification()
-                        .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                // [START_EXCLUDE]
-                                // Re-enable button
-                                findViewById(R.id.verify_email_button).setEnabled(true);
-
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(PlantMapActivity.this,
-                                            "Verification email sent to " + user.getEmail(),
-                                            Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Log.e(TAG, "sendEmailVerification", task.getException());
-                                    Toast.makeText(PlantMapActivity.this,
-                                            "Failed to send verification email.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                                // [END_EXCLUDE]
-                            }
-                        });
-                // [END send_email_verification]
-            }
-
-            private boolean validateForm() {
-                boolean valid = true;
-
-                String email = mEmailField.getText().toString();
-                if (TextUtils.isEmpty(email)) {
-                    mEmailField.setError("Required.");
-                    valid = false;
-                } else {
-                    mEmailField.setError(null);
+                auth = FirebaseAuth.getInstance();
+                FirebaseUser user = auth.getCurrentUser();
+                if(user!= null){
+                    auth.signOut();
+                    Toast.makeText(PlantMapActivity.this, "User Signed Out!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(PlantMapActivity.this, "User Not Logged in !", Toast.LENGTH_SHORT).show();
                 }
 
-                String password = mPasswordField.getText().toString();
-                if (TextUtils.isEmpty(password)) {
-                    mPasswordField.setError("Required.");
-                    valid = false;
-                } else {
-                    mPasswordField.setError(null);
-                }
-
-                return valid;
-            }
-
-            private void updateUI(FirebaseUser user) {
-                hideProgressDialog();
-                if (user != null) {
-                    mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
-                            user.getEmail(), user.isEmailVerified()));
-                    mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
-
-                    findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
-                    findViewById(R.id.email_password_fields).setVisibility(View.GONE);
-                    findViewById(R.id.signed_in_buttons).setVisibility(View.VISIBLE);
-
-                    findViewById(R.id.verify_email_button).setEnabled(!user.isEmailVerified());
-                } else {
-                    mStatusTextView.setText(R.string.signed_out);
-                    mDetailTextView.setText(null);
-
-                    findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
-                    findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
-                    findViewById(R.id.signed_in_buttons).setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void onClick(View v) {
-                int i = v.getId();
-                if (i == R.id.email_create_account_button) {
-                    createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
-                } else if (i == R.id.email_sign_in_button) {
-                    signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
-                } else if (i == R.id.sign_out_button) {
-                    signOut();
-                } else if (i == R.id.verify_email_button) {
-                    sendEmailVerification();
-                }
             }
 
 
-    /*
-    public void onMapSearch(View view) {
-        EditText locationSearch = (EditText) findViewById(R.id.geo_autocomplete);
-        String location = locationSearch.getText().toString();
-        List<Address> addressList = null;
 
-        if (location != null || !location.equals("")) {
-            Geocoder geocoder = new Geocoder(this);
-            try {
-                addressList = geocoder.getFromLocationName(location, 1);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Address address = addressList.get(0);
-            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        }
-    }
-*/
 
     /**
      * Manipulates the map once available.
@@ -600,7 +398,8 @@ public class PlantMapActivity extends AppCompatActivity implements OnMapReadyCal
     }
 
             private void addDrawerItems() {
-                String[] menuArray = { "Map View", "List View", "SignIn", "SignUp", "Reset Password" };
+
+                String[] menuArray = { "Map View", "List View", "SignIn", "SignUp", "Sign Out", "Manage Account" };
                 mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuArray);
                 mDrawerList.setAdapter(mAdapter);
 
@@ -614,22 +413,25 @@ public class PlantMapActivity extends AppCompatActivity implements OnMapReadyCal
                               case 1:
                                   Toast.makeText(PlantMapActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
                                   break;
-                              case 2: //First item
-
+                              case 2: //Third item
                                   startActivity(new Intent(PlantMapActivity.this, LoginActivity.class));
                                            //finish();
 
                                   break;
-                              case 3: //Second item
+                              case 3: //Fourth item
 
                                  startActivity(new Intent(PlantMapActivity.this, SignupActivity.class));
                                   break;
 
                                   //Toast.makeText(PlantMapActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
-                              case 4: //Third Item
-
-                                  startActivity(new Intent(PlantMapActivity.this, ResetPasswordActivity.class));
+                              case 4:
+                                  signOut();
                                   break;
+
+                              case 5:
+                                  startActivity(new Intent(PlantMapActivity.this, ManageAccountActivity.class));
+                                  break;
+
                           }
                     }
                 });
