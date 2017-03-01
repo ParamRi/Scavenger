acdec019a5f46f42d2ff06b3948df7d1e2c4ad20
Param Ri <paramri@gatech.edu>
2017-03-01 16:11:00 -0500

    trying to add navdrawer

--- app/src/main/java/com/teamgamma/scavenger/MainActivity.java
+++ app/src/main/java/com/teamgamma/scavenger/MainActivity.java
@@ -1,22 +1,24 @@
 package com.teamgamma.scavenger;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 import com.teamgamma.scavenger.dummy.DummyContent;
@@ -37,11 +39,44 @@ public class MainActivity extends AppCompatActivity  implements PlantFragment.On
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mNavigationView = (NavigationView) findViewById(R.id.naviView) ;
+
+        mFragmentManager = getSupportFragmentManager();
+        //mFragmentTransaction = mFragmentManager.beginTransaction();
+        //mFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+
+
+
+                if (menuItem.getItemId() == R.id.sign_in) {
+                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                    //fragmentTransaction.replace(R.id.containerView,new SentFragment()).commit();
+                }
+
+                if (menuItem.getItemId() == R.id.help) {
+                    FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+                    //xfragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+                }
+
+                return false;
+            }
+
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -65,6 +100,13 @@ public class MainActivity extends AppCompatActivity  implements PlantFragment.On
             }
         });
 
+        //android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, toolbar,R.string.app_name,
+                R.string.app_name);
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerToggle.syncState();
     }
 
 
